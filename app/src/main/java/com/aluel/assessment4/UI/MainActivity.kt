18e352cd43b5dd86c8ba3913d5ac7d51e6a7c523
Aluel-kuot <maryaluelkuot@gmail.com>
2023-08-24 18:48:30 +0300

    added

--- app/src/main/java/com/aluel/assessment4/UI/MainActivity.kt
+++ app/src/main/java/com/aluel/assessment4/UI/MainActivity.kt
@@ -1,44 +1,5 @@
 package com.aluel.assessment4.UI
 
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.widget.Toast
-//import androidx.activity.viewModels
-//import androidx.lifecycle.Observer
-//import androidx.recyclerview.widget.LinearLayoutManager
-//import com.aluel.assessment4.Models.PostAdapter
-//import com.aluel.assessment4.Models.Posts
-//import com.aluel.assessment4.ViewModel.PostsViewModel
-//import com.aluel.assessment4.databinding.ActivityMainBinding
-//
-//class MainActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityMainBinding
-//    private val postsViewModel:PostsViewModel by viewModels()
-//    private lateinit var postAdapter: PostAdapter
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding=ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//    }
-//    override fun onResume() {
-//        super.onResume()
-//
-//        postsViewModel.fetchPosts()
-//
-//        postsViewModel.posts.observe(
-//            this,
-//            Observer { postsList ->
-//                postAdapter = PostAdapter((postsList ?: emptyList()) as List<Posts>)
-//                binding.rvPosts.layoutManager = LinearLayoutManager(this@MainActivity)
-//                binding.rvPosts.adapter = postAdapter
-//            })
-//
-//        postsViewModel.errorLiveData.observe(this, Observer { error ->
-//            Toast.makeText(baseContext, error, Toast.LENGTH_LONG).show()
-//        })
-//    }
-//}
 
 
 import androidx.appcompat.app.AppCompatActivity
